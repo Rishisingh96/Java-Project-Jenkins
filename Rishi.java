ef98575bea5b622ce4892da3288da62ca860d104
rishisingh <rishicoding9838@gmail.com>
2025-04-08 00:26:59 +0530

    commint chage

--- Rishi.java
+++ Rishi.java
@@ -3,5 +3,19 @@ class Rishi{
         System.out.println("Hello, Rishi!");
         System.out.println("Welcome to the Java world.");
         System.out.println("Let's start coding!");
+
+        // This is a simple Java program that prints a welcome message.
+       System.out.println("This is a simple Java program that prints a welcome message.");
+        System.out.println("You can modify this code to add more functionality.");
+        System.out.println("Happy coding!");
+
+        // // Adding a simple calculation
+        // int a = 5;
+        // int b = 10;
+        // int sum = a + b;
+        // System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+
+        // // End of the program
+        // System.out.println("End of the program.");
     }
 }
