ea1ce16794f20a6c0a79552b4dad5435235fd29d
rishisingh <rishicoding9838@gmail.com>
2025-04-08 00:25:05 +0530

    Initial commit

---
+++ Rishi.java
@@ -0,0 +1,7 @@
+class Rishi{
+    public static void main(String[] args) {
+        System.out.println("Hello, Rishi!");
+        System.out.println("Welcome to the Java world.");
+        System.out.println("Let's start coding!");
+    }
+}
